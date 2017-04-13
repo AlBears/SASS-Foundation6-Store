7b3893199bb1544a9b4f3d101d8db1bd4f401c38
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-04-13 15:13:17 +0300

    fix header with magellan

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -13,8 +13,21 @@ $(window).on('changed.zf.mediaquery', function(event, newSize, oldSize) {
 
   if (newSize === 'large' && oldSize === "medium") {
     $('#main-menu ul').addClass('expanded');
-    $('#main-menu ul').removeClass('vertical');
+    //$('#main-menu ul').removeClass('vertical');
   }
+});
 
 
+
+
+var navigation = $('#main-navigation');
+var nav = new Foundation.Magellan(navigation);
+
+$(window).scroll(function() {
+  console.log(nav.scrollPos);
+  if(nav.scrollPos >= 300) {
+    $(navigation).addClass('fixed');
+  } else {
+    $(navigation).removeClass('fixed');
+  }
 })
